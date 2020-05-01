792d7707f00c5da08a44824e5be2e9eb2c88917b
marcosnqsdev <marcosnqsdev@gmail.com>
2020-04-30 21:28:18 -0300

    Config routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ const webscraperController = require('./controllers/webscraperController');
 
 const routes = express.Router();
 
-routes.get('/', webscraperController.index);
+routes.get('/', (req, res) => {
+    res.send({ server: 'YPodcastLib API its working!' });
+});
+
+routes.get('/webscraping', webscraperController.index);
 
 module.exports = routes;
