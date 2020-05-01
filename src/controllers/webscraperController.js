7f3c3febef6afc17f3760610e66b73829642eba8
marcosnqsdev <marcosnqsdev@gmail.com>
2020-04-30 21:56:14 -0300

    catching error

--- src/controllers/webscraperController.js
+++ src/controllers/webscraperController.js
@@ -3,7 +3,13 @@ const scraper = require('../services/webscraper');
 module.exports = {
     async index(req, res) {
         const url = 'https://www.podbean.com/podcast-detail/6bygw-4295e/Hipsters-Ponto-Tech-Podcast';
-        const podcastList = await scraper.scrapPodcasts(url);
+        const podcastList = [];
+
+        try {
+            podcastList = await scraper.scrapPodcasts(url);
+        } catch (err) {
+            console.error(err);
+        }
 
         res.send({ podcasts: podcastList });
     }
